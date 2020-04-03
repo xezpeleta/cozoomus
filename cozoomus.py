307ee1a2f7ac6ee7c32cfed0a47860265008fcde
Xabi Ezpeleta <xezpeleta@gmail.com>
2020-04-03 12:44:31 +0200

    Support recursive meetings (fixes #1)

--- cozoomus.py
+++ cozoomus.py
@@ -1,7 +1,8 @@
 import os
+import logging
 import json
 from zoomus import ZoomClient
-import datetime
+from datetime import datetime, timezone
 
 def user_update_type(client, user, user_type):
     user_data = {
@@ -13,16 +14,24 @@ def user_update_type(client, user, user_type):
     if response.status_code == 204:
         return True
     else:
-        print("ERROR: cannot assign/unassign the license. More info: {}".format(response.text))
+        logging.error("ERROR: cannot assign/unassign the license. More info: {}".format(response.text))
         return False
 
 def is_meeting_soon(meeting):
     try:
-        start_time = datetime.datetime.strptime(meeting['start_time'], '%Y-%m-%dT%H:%M:%SZ')
+        meeting_id = meeting['id']
     except:
-        return False
-    now = datetime.datetime.now()
+        meeting_id = meeting['occurrence_id']
+
+    try:
+        start_time = datetime.strptime(meeting['start_time'], '%Y-%m-%dT%H:%M:%SZ')
+    except:
+        raise ValueError('Invalid meeting start_time: %s' % meeting['start_time'])
+    logging.debug('is_meeting_soon({}) :: start_time = {}'.format(meeting_id, start_time))
+    #print('test: {}'.format(utc_to_local(start_time).strftime("%H:%M:%S")))
+    now = datetime.now()
     delta = abs(start_time - now).total_seconds() // 3600
+    logging.debug('is_meeting_soon({}) :: delta = {} hours'.format(meeting_id, delta))
     # Match meetings before and after TIME_DELTA
     # TODO: allow using diffent deltas (TIME_DELTA_BEFORE & TIME_DELTA_AFTER)
     if delta < TIME_DELTA:
@@ -30,9 +39,21 @@ def is_meeting_soon(meeting):
     else:
         return False
 
+def utc_to_local(utc_dt):
+    return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
+
 if __name__ == "__main__":
+    # Set log level
+    loglevel = os.getenv('LOGLEVEL', 'warning')
+    numeric_loglevel = getattr(logging, loglevel.upper(), None)
+    if not isinstance(numeric_loglevel, int):
+        raise ValueError('Invalid log level: %s' % loglevel)
+    logging.basicConfig(level=numeric_loglevel)
+    logging.info("SETTINGS :: LOGLEVEL = {}".format(numeric_loglevel))
+
     # Required enironment variables
     if "ZOOM_API_KEY" not in os.environ or "ZOOM_API_SECRET" not in os.environ:
+        logging.critical("Cannot find required environment variables")
         print("[ERROR] Cannot find required environment variables:")
         print(" - ZOOM_API_KEY")
         print(" - ZOOM_API_SECRET")
@@ -43,18 +64,27 @@ if __name__ == "__main__":
 
     # Time delta (in hours)
     # TODO: allow using diffent deltas (TIME_DELTA_BEFORE & TIME_DELTA_AFTER)
-    TIME_DELTA = int(os.getenv('TIME_DELTA', default=24))
+    TIME_DELTA = int(os.getenv('ZOOM_TIME_DELTA', default=24))
+    logging.info("SETTINGS :: TIME_DELTA = {} (hours)".format(TIME_DELTA))
 
     # Available licenses
     LICENSES = int(os.getenv('ZOOM_LICENSES', default=20))
+    logging.info("SETTINGS :: LICENSES = {}".format(LICENSES))
 
     # The following users will always keep their license
     WHITELISTED_USERS = list(os.getenv('ZOOM_WHITELISTED_USERS', default="").strip('"').split(" "))
+    logging.info("SETTINGS :: WHITELISTED_USERS = {}".format(WHITELISTED_USERS))
 
     # User type values (ZOOM API)
     USER_NON_LICENSED = 1
     USER_LICENSED = 2
 
+    # Meeting type values (ZOOM API)
+    MEETING_TYPE_INSTANT = 1
+    MEETING_TYPE_SCHEDULED = 2
+    MEETING_TYPE_RECURRING_WITHOUT_TIME = 3
+    MEETING_TYPE_RECURRING_WITH_TIME = 8
+    
     client = ZoomClient(API_KEY, API_SECRET)
     users = json.loads(client.user.list().content)['users']
 
@@ -65,8 +95,7 @@ if __name__ == "__main__":
                 user_update_type(client, user, USER_LICENSED)
                 print("[%s] User whitelisted. License assigned" % user['email'])
             else:
-                #print("[%s] User whitelisted. Nothing to do, already licensed" % user['email'])
-                pass
+                logging.debug("[%s] User whitelisted. Nothing to do, already licensed" % user['email'])
             required_licenses += 1
             continue
         try:
@@ -75,27 +104,54 @@ if __name__ == "__main__":
             continue
         
         for meeting in meetings:
-            if is_meeting_soon(meeting):
-                #print("-%s (%s)" % (meeting['topic'], meeting['start_time']))
+            if meeting['type'] == MEETING_TYPE_RECURRING_WITHOUT_TIME:
+                # Recurring meeting, with no fixed time
                 if user['type'] == USER_NON_LICENSED:
                     user_update_type(client, user, USER_LICENSED)
-                    print("[%s] Meetings scheduled. License assigned" % user['email'])
+                    print("[%s] Recurring meeting with no fixed time. License assigned" % user['email'])
                 else:
-                    print("[%s] Meetings scheduled. Nothing to do, already licensed" % user['email'])
+                    print("[%s] Recurring meeting with no fixed time. Nothing to do, already licensed" % user['email'])
                 required_licenses += 1
                 break
+            if meeting['type'] == MEETING_TYPE_RECURRING_WITH_TIME:
+                # Recurring meeting with fixed time
+                ## We need to query for meeting occurrences
+                ## and in order to do that we load "full" meeting info
+                meeting = json.loads(client.meeting.get(id=meeting['id']).content)
+                if 'occurrences' in meeting:
+                    for occurrence in meeting['occurrences']:
+                        if is_meeting_soon(occurrence):
+                            if user['type'] == USER_NON_LICENSED:
+                                user_update_type(client, user, USER_LICENSED)
+                                print("[%s] Recurring meetings scheduled. License assigned" % user['email'])
+                            else:
+                                print("[%s] Recurring meetings scheduled. Nothings to do, already licensed" % user['email'])
+                            required_licenses += 1
+                            break
+                    else:
+                        continue # all occurrences are far
+                    break # there is a close occurrence
+            else:
+                # Scheduled meeting or instant meeting
+                if is_meeting_soon(meeting):
+                    if user['type'] == USER_NON_LICENSED:
+                        user_update_type(client, user, USER_LICENSED)
+                        print("[%s] Meetings scheduled. License assigned" % user['email'])
+                    else:
+                        print("[%s] Meetings scheduled. Nothing to do, already licensed" % user['email'])
+                    required_licenses += 1
+                    break
         else:
             if user['type'] == USER_LICENSED:
                 user_update_type(client, user, USER_NON_LICENSED)
                 print("[%s] No meetings scheduled. License removed" % user['email'])
             else:
-                #print("No meetings scheduled. Nothing to do")
-                pass
+                logging.debug("[%s] No meetings scheduled. Nothing to do" % user['email'])
 
     print("\n")
     print("~ SUMMARY ~")
     print("Users: %i" % len(users))
     print("Whitelisted users: %i" % len(WHITELISTED_USERS))
-    print("Users with scheduled meetings: %i" % (required_licenses - len(WHITELISTED_USERS)))
+    print("Users with meetings: %i" % (required_licenses - len(WHITELISTED_USERS)))
     print("Assigned licenses: %i/%i" % (required_licenses, LICENSES))
     print("Available licenses: %i/%i" % ((LICENSES - required_licenses), LICENSES))
